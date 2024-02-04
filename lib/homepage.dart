4500f79b8f1a6420ab57a9a07a94f4cec410f0d4
Shivam rathor <shivamrathor23215@gmail.com>
2024-02-04 21:56:37 +0530

    complete

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:allen/feature_list.dart';
 import 'package:allen/open_ai_service.dart';
 import 'package:allen/pallete.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
@@ -15,13 +16,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final speechToText = SpeechToText();
-  FlutterTts flutterTts = FlutterTts();
+  final flutterTts = FlutterTts();
+  String lastWords = '';
+  final OpenAIService openAIService = OpenAIService();
+  String? generatedContent;
+  String? generatedImageUrl;
+  int start = 200;
+  int delay = 200;
 
-  final OpenAiService openAiService = OpenAiService();
-  String lastWords = "";
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     initSpeechToText();
     initTextToSpeech();
@@ -37,31 +41,24 @@ class _HomePageState extends State<HomePage> {
     setState(() {});
   }
 
-  /// Each time to start a speech recognition session
   Future<void> startListening() async {
     await speechToText.listen(onResult: onSpeechResult);
     setState(() {});
   }
 
-  /// Manually stop the active speech recognition session
-  /// Note that there are also timeouts that each platform enforces
-  /// and the SpeechToText plugin supports setting timeouts on the
-  /// listen method.
   Future<void> stopListening() async {
     await speechToText.stop();
     setState(() {});
   }
 
-  /// This is the callback that the SpeechToText plugin calls when
-  /// the platform returns recognized words.
   void onSpeechResult(SpeechRecognitionResult result) {
     setState(() {
       lastWords = result.recognizedWords;
     });
   }
 
-  Future<void> systemSpeak(String Content) async {
-    await flutterTts.speak(Content);
+  Future<void> systemSpeak(String content) async {
+    await flutterTts.speak(content);
   }
 
   @override
@@ -75,107 +72,174 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Allen'),
+        title: BounceInDown(
+          child: const Text('Allen'),
+        ),
         leading: const Icon(Icons.menu),
         centerTitle: true,
       ),
-      // voice chat Image
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Stack(
-              children: [
-                Center(
-                  child: Container(
-                    height: 120,
-                    width: 120,
-                    margin: const EdgeInsets.only(top: 4),
-                    decoration: const BoxDecoration(
+            // virtual assistant picture
+            ZoomIn(
+              child: Stack(
+                children: [
+                  Center(
+                    child: Container(
+                      height: 120,
+                      width: 120,
+                      margin: const EdgeInsets.only(top: 4),
+                      decoration: const BoxDecoration(
                         color: Pallete.assistantCircleColor,
-                        shape: BoxShape.circle),
-                  ),
-                ),
-                Center(
-                  child: Container(
-                    height: 123,
-                    width: 123,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/virtualAssistant.png'),
+                        shape: BoxShape.circle,
                       ),
                     ),
                   ),
-                )
-              ],
+                  Container(
+                    height: 123,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/images/virtualAssistant.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
             // chat bubble
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Pallete.borderColor),
-                  borderRadius:
-                      BorderRadius.circular(20).copyWith(topLeft: Radius.zero)),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: Text(
-                  'Good Morning, What task Can i do for you ?',
-                  style: TextStyle(
+            FadeInRight(
+              child: Visibility(
+                visible: generatedImageUrl == null,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
+                  ),
+                  margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+                    top: 30,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Pallete.borderColor,
+                    ),
+                    borderRadius: BorderRadius.circular(20).copyWith(
+                      topLeft: Radius.zero,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Text(
+                      generatedContent == null
+                          ? 'Good Morning, what task can I do for you?'
+                          : generatedContent!,
+                      style: TextStyle(
+                        fontFamily: 'Cera Pro',
+                        color: Pallete.mainFontColor,
+                        fontSize: generatedContent == null ? 25 : 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            if (generatedImageUrl != null)
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(generatedImageUrl!),
+                ),
+              ),
+            SlideInLeft(
+              child: Visibility(
+                visible: generatedContent == null && generatedImageUrl == null,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(top: 10, left: 22),
+                  child: const Text(
+                    'Here are a few features',
+                    style: TextStyle(
+                      fontFamily: 'Cera Pro',
                       color: Pallete.mainFontColor,
-                      fontSize: 25,
-                      fontFamily: 'Cera Pro'),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
             ),
-
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.only(top: 10, left: 22),
-              child: const Text(
-                "Here are a new commands",
-                style: TextStyle(
-                  fontFamily: "Cera Pro",
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Pallete.mainFontColor,
-                ),
+            // features list
+            Visibility(
+              visible: generatedContent == null && generatedImageUrl == null,
+              child: Column(
+                children: [
+                  SlideInLeft(
+                    delay: Duration(milliseconds: start),
+                    child: const FeatureList(
+                      color: Pallete.firstSuggestionBoxColor,
+                      headerText: 'ChatGPT',
+                      descriptionText:
+                          'A smarter way to stay organized and informed with ChatGPT',
+                    ),
+                  ),
+                  SlideInLeft(
+                    delay: Duration(milliseconds: start + delay),
+                    child: const FeatureList(
+                      color: Pallete.secondSuggestionBoxColor,
+                      headerText: 'Dall-E',
+                      descriptionText:
+                          'Get inspired and stay creative with your personal assistant powered by Dall-E',
+                    ),
+                  ),
+                  SlideInLeft(
+                    delay: Duration(milliseconds: start + 2 * delay),
+                    child: const FeatureList(
+                      color: Pallete.thirdSuggestionBoxColor,
+                      headerText: 'Smart Voice Assistant',
+                      descriptionText:
+                          'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+                    ),
+                  ),
+                ],
               ),
-            ),
-            //feature list
-            const FeatureList(
-              color: Pallete.firstSuggestionBoxColor,
-              headerText: 'Chat GPT',
-              descriptionText: 'Make  search Your searching faster and large',
-            ),
-            const FeatureList(
-              color: Pallete.secondSuggestionBoxColor,
-              headerText: 'alexa',
-              descriptionText: 'Make  search Your searching faster and large',
-            ),
-            const FeatureList(
-              color: Pallete.thirdSuggestionBoxColor,
-              headerText: 'Dellby',
-              descriptionText: 'Make  search Your searching faster and large',
-            ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          if (await speechToText.hasPermission && speechToText.isNotListening) {
-            startListening();
-          } else if (await speechToText.isListening) {
-            final speech = await openAiService.isAtPromptAPI(lastWords);
-            systemSpeak(speech);
-            stopListening();
-          } else {
-            initSpeechToText();
-          }
-        },
-        child: const Icon(Icons.mic),
+      floatingActionButton: ZoomIn(
+        delay: Duration(milliseconds: start + 3 * delay),
+        child: FloatingActionButton(
+          backgroundColor: Pallete.firstSuggestionBoxColor,
+          onPressed: () async {
+            if (await speechToText.hasPermission &&
+                speechToText.isNotListening) {
+              await startListening();
+            } else if (speechToText.isListening) {
+              final speech = await openAIService.isArtPromptAPI(lastWords);
+              if (speech.contains('https')) {
+                generatedImageUrl = speech;
+                generatedContent = null;
+                setState(() {});
+              } else {
+                generatedImageUrl = null;
+                generatedContent = speech;
+                setState(() {});
+                await systemSpeak(speech);
+              }
+              await stopListening();
+            } else {
+              initSpeechToText();
+            }
+          },
+          child: Icon(
+            speechToText.isListening ? Icons.stop : Icons.mic,
+          ),
+        ),
       ),
     );
   }
--- lib/open_ai_service.dart
+++ lib/open_ai_service.dart
@@ -1,30 +1,29 @@
 import 'dart:convert';
 
+
 import 'package:allen/secrets.dart';
 import 'package:http/http.dart' as http;
 
-final List<Map<String, String>> messages = [];
+class OpenAIService {
+  final List<Map<String, String>> messages = [];
 
-class OpenAiService {
-  // final List<Map<String, String>> messages = [];
-
-  Future<String> isAtPromptAPI(String prompt) async {
+  Future<String> isArtPromptAPI(String prompt) async {
     try {
       final res = await http.post(
         Uri.parse('https://api.openai.com/v1/chat/completions'),
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer $openAiAPIKey'
+          'Authorization': 'Bearer $openAiAPIKey',
         },
         body: jsonEncode({
           "model": "gpt-3.5-turbo",
           "messages": [
             {
-              "role": "user",
-              "content":
-                  "Does this message wants to generate AI picture, image,art or anything similar ? $prompt . Simply answer with yes or no. ",
-            },
-          ]
+              'role': 'user',
+              'content':
+                  'Does this message want to generate an AI picture, image, art or anything similar? $prompt . Simply answer with a yes or no.',
+            }
+          ],
         }),
       );
       print(res.body);
@@ -38,79 +37,85 @@ class OpenAiService {
           case 'yes':
           case 'Yes.':
           case 'yes.':
-            final res = await DallEAPI(prompt);
+            final res = await dallEAPI(prompt);
             return res;
           default:
-            final res = await ChatGPTAPI(prompt);
+            final res = await chatGPTAPI(prompt);
             return res;
         }
       }
-      return "An Internal error occur";
+      return 'An internal error occurred';
     } catch (e) {
       return e.toString();
     }
   }
 
-  Future<String> ChatGPTAPI(String prompt) async {
-    messages.add({'role': 'user', 'content': prompt});
+  Future<String> chatGPTAPI(String prompt) async {
+    messages.add({
+      'role': 'user',
+      'content': prompt,
+    });
     try {
       final res = await http.post(
         Uri.parse('https://api.openai.com/v1/chat/completions'),
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer $openAiAPIKey'
+          'Authorization': 'Bearer $openAiAPIKey',
         },
         body: jsonEncode({
           "model": "gpt-3.5-turbo",
           "messages": messages,
         }),
       );
-      print(res.body);
+
       if (res.statusCode == 200) {
         String content =
             jsonDecode(res.body)['choices'][0]['message']['content'];
         content = content.trim();
 
         messages.add({
-          'role': 'assitant',
+          'role': 'assistant',
           'content': content,
         });
         return content;
       }
-      return "An Internal error occur";
+      return 'An internal error occurred';
     } catch (e) {
       return e.toString();
     }
   }
-}
 
-Future<String> DallEAPI(String prompt) async {
-  messages.add({'role': 'user', 'content': prompt});
-  try {
-    final res = await http.post(
-      Uri.parse('https://api.openai.com/v1/images/generations'),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $openAiAPIKey'
-      },
-      body: jsonEncode({
-        'prompt': prompt,
-        'n': 1,
-      }),
-    );
-    print(res.body);
-    if (res.statusCode == 200) {
-      String imageUrl = jsonDecode(res.body)['data'][0]['url'];
-      imageUrl = imageUrl.trim();
- 
-      messages.add({
-        'role': 'assitant',
-        'content': imageUrl,
-      });
-      return imageUrl;
+  Future<String> dallEAPI(String prompt) async {
+    messages.add({
+      'role': 'user',
+      'content': prompt,
+    });
+    try {
+      final res = await http.post(
+        Uri.parse('https://api.openai.com/v1/images/generations'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $openAiAPIKey',
+        },
+        body: jsonEncode({
+          'prompt': prompt,
+          'n': 1,
+        }),
+      );
+
+      if (res.statusCode == 200) {
+        String imageUrl = jsonDecode(res.body)['data'][0]['url'];
+        imageUrl = imageUrl.trim();
+
+        messages.add({
+          'role': 'assistant',
+          'content': imageUrl,
+        });
+        return imageUrl;
+      }
+      return 'An internal error occurred';
+    } catch (e) {
+      return e.toString();
     }
-    return "An Internal error occur";
-  } catch (e) {
-    return e.toString();
   }
 }
